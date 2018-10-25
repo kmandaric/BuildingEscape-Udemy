27ca1bb6e450b67ee15e0e90a66f618fd435cecd
Kristian Mandarić <mandaric.kristian@gmail.com>
2018-10-25 14:01:56 +0200

    Getting ActorName and Position

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -2,6 +2,7 @@
 
 #include "PositionReport.h"
 #include "BuildingEscape.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -11,7 +12,7 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+	
 }
 
 
@@ -20,7 +21,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("Object %s is at %s!"), *ObjectName, *ObjectPos);
 	
 }
 
